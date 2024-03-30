588a0048e0bcd25372efa56327fddc18a8c6e09d
Max R <rgmasim@gmail.com>
2024-03-30 09:49:10 +0700

    Implement wallet balance check.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # play-web3
+## Installation
+Create virtual environment:
+```bash
+python3 -m venv .venv
+```
+Install dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+## How to run
+```bash
+WALLET_ADDRESS=<wallet address starting with 0x> RPC_URL=<rpc server url> python src/main.py
+```
+RPC server url may be taken at [chainlist](https://chainlist.org/?search=line).
+
+## Credits
+ - [Web3.0 на Python, часть 1: основы](https://habr.com/ru/articles/674204/) (Russian)
---
+++ src/main.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from web3 import Web3
+
+rpc_url = os.environ.get('RPC_URL')
+if not rpc_url:
+    sys.exit('RPC_URL must be set')
+
+wallet_address = os.environ.get('WALLET_ADDRESS') 
+if not wallet_address:
+    sys.exit('WALLET_ADDRESS must be set')
+
+web3 = Web3(Web3.HTTPProvider(rpc_url))
+print(f"Is connected: {web3.is_connected()}")
+
+print(f"gas price: {web3.eth.gas_price} BNB")  # кол-во Wei за единицу газа
+print(f"current block number: {web3.eth.block_number}")
+print(f"number of current chain is {web3.eth.chain_id}")
+
+balance = web3.eth.get_balance(wallet_address)
+print(f"Wallet {wallet_address}:")
+print(f"balance: {balance}")
+
+ether_balance = Web3.from_wei(balance, 'ether')
+print(f"eth balance: {ether_balance}")
