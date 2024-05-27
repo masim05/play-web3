43893988deb15a15718e5c782f55594d7a2fb968
Max R <rgmasim@gmail.com>
2024-05-27 19:25:53 +0700

    Add more tests.

--- utils/test/endpoints.sh
+++ utils/test/endpoints.sh
@@ -16,16 +16,29 @@ test "https://rpc.alignedlayer.crptmax.com/"
 test "https://api.alignedlayer.crptmax.com/"
 test "https://rpc.alignedlayer-t.crptmax.com/"
 test "https://api.alignedlayer-t.crptmax.com/"
+wscat --connect wss://rpc.alignedlayer-t.crptmax.com/websocket \
+-x '{ "jsonrpc": "2.0", "method": "status", "id": 1 }' > /dev/null
 
 test "https://rpc.galactica.crptmax.com/"
 test "https://api.galactica.crptmax.com/"
 test "https://rpc.galactica-t.crptmax.com/"
 test "https://api.galactica-t.crptmax.com/"
+wscat --connect wss://rpc.galactica-t.crptmax.com/websocket \
+-x '{ "jsonrpc": "2.0", "method": "status", "id": 1 }' > /dev/null
 
 test "https://rpc.0g.crptmax.com/"
 test "https://api.0g.crptmax.com/" 501
 test "https://rpc.0g-t.crptmax.com/"
 test "https://api.0g-t.crptmax.com/" 501
+wscat --connect wss://rpc.0g-t.crptmax.com/websocket \
+-x '{ "jsonrpc": "2.0", "method": "status", "id": 1 }' > /dev/null
 
 test "https://rpc.band.crptmax.com/"
 test "https://api.band.crptmax.com/" 501
+wscat --connect wss://rpc.band.crptmax.com/websocket \
+-x '{ "jsonrpc": "2.0", "method": "status", "id": 1 }' > /dev/null
+
+grpcurl grpc.band.crptmax.com:26443 list > /dev/null;
+grpcurl grpc.galactica-t.crptmax.com:22443 list > /dev/null;
+grpcurl grpc.alignedlayer-t.crptmax.com:23443 list > /dev/null;
+grpcurl grpc.0g-t.crptmax.com:19443 list > /dev/null;
