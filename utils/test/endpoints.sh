7e934d13d18a2079871508ef723a22f1bdba6a2d
Max R <rgmasim@gmail.com>
2025-08-24 07:46:29 +0700

    Cleanup endpoints to check.

--- utils/test/endpoints.sh
+++ utils/test/endpoints.sh
@@ -26,33 +26,15 @@ test_grpc () {
 }
 
 printf "HTTP endpoints\n"
-#test_http "https://rpc.alignedlayer.crptmax.com/"
-#test_http "https://api.alignedlayer.crptmax.com/"
-test_http "https://rpc.alignedlayer-t.crptmax.com/"
-test_http "https://api.alignedlayer-t.crptmax.com/"
 
-#test_http "https://rpc.galactica.crptmax.com/"
-#test_http "https://api.galactica.crptmax.com/"
-test_http "https://rpc.galactica-t.crptmax.com/"
-test_http "https://api.galactica-t.crptmax.com/"
-
-#test_http "https://rpc.0g.crptmax.com/"
-#test_http "https://api.0g.crptmax.com/" 501
-test_http "https://rpc.0g-t.crptmax.com/"
-test_http "https://api.0g-t.crptmax.com/" 501
-
-test_http "https://rpc.band.crptmax.com/"
-test_http "https://api.band.crptmax.com/" 501
+test_http "https://rpc.arkeo.crptmax.com/"
+test_http "https://rpc.arkeo-t.crptmax.com/"
 
 printf "\nWS endpoints\n"
-test_ws "rpc.band.crptmax.com"
-test_ws "rpc.alignedlayer-t.crptmax.com"
-test_ws "rpc.galactica-t.crptmax.com"
-test_ws "rpc.0g-t.crptmax.com"
+test_ws "rpc.arkeo.crptmax.com"
+test_ws "rpc.arkeo-t.crptmax.com"
 
 printf "\ngRPC endpoints\n"
-test_grpc "grpc.band.crptmax.com:27443"
-test_grpc "grpc.galactica-t.crptmax.com:20443"
-test_grpc "grpc.alignedlayer-t.crptmax.com:23443"
-test_grpc "grpc.0g-t.crptmax.com:21443"
+test_grpc "grpc.arkeo.crptmax.com:22443"
+test_grpc "grpc.arkeo-t.crptmax.com:21443"
 
